7df9bb832b086074d254940246f8ae6cc441c1d6
Caleb B <Caleb.Barnett0@gmail.com>
2024-07-11 19:02:08 -0500

    1.2

--- Rust/Arrays_and_Strings/check_permutation/src/main.rs
+++ Rust/Arrays_and_Strings/check_permutation/src/main.rs
@@ -1,3 +1,34 @@
+
+//sort string
+fn sort_string(s: &str) -> String {
+    let mut chars: Vec<char> = s.chars().collect(); 
+    chars.sort(); 
+    chars.into_iter().collect()
+}
+
+//check permutation
+fn check_permutation(a: &str, b: &str) -> bool {
+    if a.len() != b.len() {
+        return false
+    }
+    sort_string(a) == sort_string(b)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test_str_one = "dog";
+    let test_str_two = "god";
+    println!("'{}' and '{}' are permutations: {}", test_str_one, test_str_two, check_permutation(test_str_one, test_str_two));
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn permutation_test() {
+        assert_eq!(check_permutation("hello", "world"), false);
+        assert_eq!(check_permutation("dog", "god"), true);
+    }
 }
---
+++ Rust/Arrays_and_Strings/check_permutation/target/debug/deps/check_permutation-22193df5a41f59f2.d
@@ -0,0 +1,5 @@
+/Users/calebbarnett/GitHub/CrackingTheCodingInterview/Rust/Arrays_and_Strings/check_permutation/target/debug/deps/check_permutation-22193df5a41f59f2: src/main.rs
+
+/Users/calebbarnett/GitHub/CrackingTheCodingInterview/Rust/Arrays_and_Strings/check_permutation/target/debug/deps/check_permutation-22193df5a41f59f2.d: src/main.rs
+
+src/main.rs:
